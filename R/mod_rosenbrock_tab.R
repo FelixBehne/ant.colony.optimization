e6dbf827d747898fb2c2c211fadd0b3217851968
Arfarin <Engelmayer@stud.dhbw-ravensburg.de>
2021-06-01 03:30:16 +0200

    missing bracket added

--- R/mod_rosenbrock_tab.R
+++ R/mod_rosenbrock_tab.R
@@ -68,8 +68,9 @@ mod_rosenbrock_tab_server <- function(id, input_c) {
     })
     # output element to show actual minimum of the Rosenbrock function 
     output$result_actual <- shiny::renderTable(minima_rosenbrock)
-    })
-  # output element to show the minimum calculated by ACO (with package evoper)
+    
+    
+    # output element to show the minimum calculated by ACO (with package evoper)
     output$result_aco <- shiny::renderTable({
       calculate_min(
         iter = input_c$iterations,
@@ -78,5 +79,8 @@ mod_rosenbrock_tab_server <- function(id, input_c) {
         fu = "rosenbrock"
       )
     })
+    
+    })
+
     
 }
